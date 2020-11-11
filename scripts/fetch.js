a56e40dea49bc4a57c511053d7ac6a12994b6466
Osama-you <osama.w.younis@gmail.com>
2020-11-11 09:16:34 +0200

    create getPopularVideos Relates #10

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -63,3 +63,17 @@ const getSearchResults = (input = "", callback, maxResults = 10) => {
     callback
   );
 };
+
+// get the Popular Videos result by regionCode
+const getPopularVideos = (callback, regionCode = "JP", maxResults = 10) => {
+  // set the setting for search operating
+  getData(
+    "videos",
+    {
+      maxResults: maxResults,
+      chart: "mostPopular",
+      regionCode: regionCode,
+    },
+    callback
+  );
+};
